5b16da54dddea3d42404cc5d3209cb8cd74445b1
Ankit Das <75267634+meankitdas@users.noreply.github.com>
2022-12-21 17:31:06 +0530

    initial-4

---
+++ .history/components/mainPage/mainPage_20221221172109.js
@@ -0,0 +1,29 @@
+import Stars from "./stars";
+import MainContent from "./maincontent";
+import Navbar from "./navbar";
+
+
+export default function MainPage() {
+  return (
+    <section className=" min-h-screen p-2 " id="about">
+      {/* we will suspend it in small screen */}
+
+      <nav className="bg-white min-w-screen h-14 mx-10 rounded-2xl  text-black text-sm overflow-hidden hidden lg:block md:block">
+        <Navbar />
+      </nav>
+
+      {/* stars image  */}
+      <Stars />
+
+      {/* main content */}
+
+      <MainContent />
+
+      {/* live section */}
+
+      <div className="flex justify-center items-center h-24 overflow-hidden md:m-32 md:mb-5 m-10 bg-white rounded-2xl ">
+        <h1 className="md:text-2xl text-base">** Live Registration</h1>
+      </div>
+    </section>
+  );
+}
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bde9443a9285fcbc"; }
+/******/ 		__webpack_require__.h = function() { return "240d03494186c568"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
